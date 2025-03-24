d15b9eaf62cd7bd6eb27094dcbf1f0b7132a0689
Ahmed Abdulkarim <ahmedaldiabdev@gmail.com>
2025-03-24 20:30:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Maze 
+{     
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AAD Sends Regards");
+            Console.ReadKey();
+        }
+    }
+}
